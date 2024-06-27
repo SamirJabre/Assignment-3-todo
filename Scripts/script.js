cd2d119bfab334df854b5657ae5161d5770104f4
Samir Jabre <samirjabre8@gmail.com>
2024-06-27 22:56:39 +0300

    finished the task configration

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,16 +1,33 @@
 let tasks=0;
 let texting = document.getElementById("texting");
 let availableTasks=document.getElementById("available-tasks");
-
+let submitBtn=document.getElementById("submit-btn");
 let homeBtn=document.getElementById("home-btn");
-
+let taskName=document.getElementById("task-input");
+let description=document.getElementById("description");
+let assign=document.getElementById("assign-task");
+let date=document.getElementById("duedate");
 
 let addTaskBtn=document.getElementById("add-btn");
 let tasksList=document.querySelector("ul");
 
 if(tasks>0){
-    texting.innerText="test"
+    texting.innerText="test";
 }
+submitBtn.addEventListener("click",function(){
+    if(taskName.value != "" && description.value!="" && date.value!=""){
+        tasks++;
+        console.log("all is empty");
+        // let newTask=document.createElement("li")
+        // newTask.innerText=taskName.value
+        // tasksList.append(newTask)
+        tasksList.innerHTML+= `<li><p>#${tasks}</p>${taskName.value} <br><p class="date-config">${date.value}</p> <span class="status"></span></li>`
+        taskName.value="";
+        description.value="";
+        date.value="";
+        assign.value="";
+    }
+})
 
 
 addTaskBtn.addEventListener("click", function(){
@@ -34,10 +51,11 @@ addTaskBtn.addEventListener("click", function(){
     // // tasksList.append(newTask)
     // tasksList.innerHTML+= `<li><p>#1</p> Task 1s <p>date</p> <span class="status"></span></li>`
 })
-homeBtn.addEventListener("click",function(){
+
+function homie(){
     texting.classList.remove("texting");
     availableTasks.classList.add("available-tasks");
     if(tasks>0){
-        texting.innerText="test"
+        texting.innerText="FINISH YOUR TASKS !!"
     }
-})
+}
